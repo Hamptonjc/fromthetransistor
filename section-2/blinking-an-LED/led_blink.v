30613f37fb4da22d22c76d75d483e071d3745330
Jonathan Hampton <hamptonjc221@gmail.com>
2021-08-18 20:16:41 -0500

    finish LED blink verilog script

--- section-2/blinking-an-LED/led_blink.v
+++ section-2/blinking-an-LED/led_blink.v
@@ -1,4 +1,9 @@
 
+
+:x
+
+
+
 module led_blink(i_clock,
 		 i_enable,
 		 i_switch_1,
@@ -39,11 +44,76 @@ module led_blink(i_clock,
 	wire w_LED_SELECT;
 
 begin
+	// 100hz process
 	always @ (posedge i_clock)
 		begin
+			if (r_CNT_100HZ == c_CNT_100HZ - 1)
+				begin
+					r_TOGGLE_100HZ <= !r_TOGGLE_100HZ;
+					r_CNT_100HZ <= 0;
+				end
+			else
+				r_CNT_100HZ <= r_CNT_100HZ + 1;
+		end
+
+	// 50hz process
+	always @ (posedge i_clock)
+		begin
+			if (r_CNT_50HZ == c_CNT_50HZ-1)
+				begin
+					r_TOGGLE_50HZ <= !r_TOGGLE_50HZ;
+					r_CNT_50HZ <= 0;
+				end
+			else
+				r_CNT_50HZ <= r_CNT_50HZ + 1;
+		end
+
+	// 10hz process
+	always @ (posedge i_clock)
+		begin
+			if (r_CNT_10HZ == c_CNT_10HZ - 1)
+				begin
+					r_TOGGLE_10HZ <= !r_TOGGLE_10HZ;
+					r_CNT_10HZ <= 0;
+				end
+			else
+				r_CNT_10HZ <= r_CNT_10HZ + 1;
+		end
+
+	// 1hz process
+	always @ (posedge i_clock)
+		begin
+			if (r_CNT_1HZ == c_CNT_1HZ-1)
+				begin
+					r_TOGGLE_1HZ <= !r_TOGGLE_1HZ;
+					r_CNT_1HZ <= 0;
+				end
+			else
+				r_CNT_1HZ <= r_CNT_1HZ + 1;
+		end
 
 
-	
+	// Multiplexer based on switch inputs 
+	always @ (*)
+	  begin
+			case ({i_switch_1, i_switch_2})
+				2'b11 : r_LED_SELECT <= r_TOGGLE_1HZ;
+				2'b10 : r_LED_SELECT <= r_TOGGLE_10HZ;
+				2'b01 : r_LED_SELECT <= r_TOGGLE_50HZ;
+				2'b00 : r_LED_SELECT <= r_TOGGLE_100HZ;
+			endcase
+		end
+
+		assign o_led_drive = r_LED_SELECT & i_enable;
+	end
+	endmodule
+
+
+
+
+
+
+
 
 
 
